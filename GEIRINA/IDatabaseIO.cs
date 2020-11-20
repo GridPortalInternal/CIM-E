376fa8c4fd359f602491fa65665ff3e07097e9ca
Chujie Zeng <chujie.zeng@gmail.com>
2020-11-20 15:01:18 -0500

    Add GEIRINA related interface

---
+++ GEIRINA/IDatabaseIO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIME.GEIRINA
+{
+    public interface IDatabaseIO
+    {
+        void CreateTable(IDatabaseConnection conn);
+
+        void InsertInto(IDatabaseConnection conn);
+    }
+}
