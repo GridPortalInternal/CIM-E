636880635a96b1a421b23acf6a6dc4e47dacab51
Chujie Zeng <chujie.zeng@gmail.com>
2020-11-20 14:46:42 -0500

    Add two new classes

---
+++ Wires/ACLineDot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIME.Wires
+{
+    /// <summary>
+    /// New Class in CIM/E
+    /// </summary>
+    public class ACLineDot : Conductor
+    {
+    }
+}
---
+++ Wires/TapChangerType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIME.Wires
+{
+    /// <summary>
+    /// Replace Enum TapChangerKind in CIM
+    /// </summary>
+    public class TapChangerType : Core.IdentifiedObject
+    {
+    }
+}
